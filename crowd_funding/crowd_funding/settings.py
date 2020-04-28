7c0cdaf72a66d068ef39e02687c09a95a3353d02
AliIsmail <aliismail@localhost.localdomain>
2020-04-28 22:46:32 +0200

    fixing bugs

--- crowd_funding/crowd_funding/settings.py
+++ crowd_funding/crowd_funding/settings.py
@@ -122,7 +122,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'crowdfunding',
         'USER': 'root',
-        'PASSWORD': 'rootroot',
+        'PASSWORD': 'R12!dff2svF0',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
