b13dbcb84784f7b614a102fcd49afd629c6ed29e
shimaaibrahim1 <ir.shaimaa@gmail.com>
2020-04-28 23:37:19 +0200

    selected to show table

--- crowd_funding/projects/admin.py
+++ crowd_funding/projects/admin.py
@@ -1,5 +1,38 @@
 from django.contrib import admin
-from .models import Category, SelectedToShow
+from django.contrib.admin import AdminSite
+from .models import *
+from django.template.response import TemplateResponse
+from django.urls import path
+from .views import *
+from django.contrib import messages
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(SelectedToShow)
+admin.site.register(Project)
+admin.site.register(ProjectPictures)
+admin.site.register(Comment)
+admin.site.register(Tag)
+admin.site.register(Reply)
+admin.site.register(Donate)
+admin.site.register(ReportProject)
+admin.site.register(ReportComment)
+
+
+
+class SelectedToShowAdmin(admin.ModelAdmin):
+
+    def save_model(self, request, obj, form, change):
+        if SelectedToShow.objects.all().count() >1: 
+            messages.set_level(request, messages.ERROR)
+            messages.error(request, 'Featured cannot be more than 5 projects....')
+            return
+        elif SelectedToShow.objects.filter(project=obj.project.id).exists():
+            messages.set_level(request, messages.ERROR)
+            messages.error(request, 'Project already Exists....')
+            return
+        else:
+            obj.save()
+            return super(SelectedToShowAdmin, self).save_model(request, obj, form, change)
+
+
+# admin.site.unregister(SelectedToShow)
+admin.site.register(SelectedToShow, SelectedToShowAdmin)
